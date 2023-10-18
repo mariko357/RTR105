1e582f1b18625fd913903067d0783c4d2da8f109
mariko357 <marko@markbel.dev>
2023-10-18 11:49:35 +0300

    📒 Created multi-source code, used make.

---
+++ multifile-code/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(){
+	printf("Starting execution of the program (this is from main.c)\n");
+	printHello();
+	return 0;
+}
---
+++ multifile-code/somePrint.c
@@ -0,0 +1,4 @@
+#include "main.h"
+void printHello(){
+	printf("Hello from second file (somePrint.c)\n");
+}
